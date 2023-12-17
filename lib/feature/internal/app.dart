0b3ff72b039daf3c5c30c7cc92fc59845e2b3de5
Sejimu <snursultan322@gmail.com>
2023-12-17 21:03:10 +0600

    project structure

---
+++ lib/feature/internal/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:neobis_challenge/feature/presentation/themes/app_color.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: AppColors.scaffoldBgColor),
+      home: Scaffold(
+        body: Text("hello world"),
+      ),
+    );
+  }
+}
